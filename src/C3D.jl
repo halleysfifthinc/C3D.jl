ea484c18f987735594283ee8e32d388358042eec
Allen Hill <allenofthehills@gmail.com>
2018-06-13 18:57:52 -0400

    Refactor the handling of endianness and VAX types
    This solves a deprecation regarding globals and also avoids the
    antipattern of using globals in the first place.

--- src/C3D.jl
+++ src/C3D.jl
@@ -4,8 +4,7 @@ module C3D
 
 using VaxData
 
-const LE = 1
-const BE = 2
+@enum Endian LE=1 BE=2
 
 F_ENDIAN = LE
 VAX = false
@@ -86,31 +85,31 @@ Base.getindex(f::C3DFile, key) = getindex(f.bylabels, key)
 
 relseek(s::IOStream, n::Int) = seek(s, position(s) + n)
 
-function readheader(f::IOStream)::Dict{Symbol,Any}
+function readheader(f::IOStream, FEND::Endian, FType::Type{T}) where T <: Union{Float32,VaxFloatF}
     seek(f,0)
-    paramptr = saferead(f,Int8)
-    read(f,Int8)
-    numpoints = saferead(f,Int16)
-    numanalog = saferead(f,Int16)
-    firstframe = saferead(f,Int16)
-    lastframe = saferead(f,Int16)
-    maxinterp = saferead(f,Int16)
-    pointscale = saferead(f,Float32)
-    DATA_START = saferead(f,Int16)
-    analogpframe = saferead(f,Int16)
-    framerate = saferead(f,Float32)
+    paramptr = saferead(f, Int8, FEND)
+    read(f, Int8)
+    numpoints = saferead(f, Int16, FEND)
+    numanalog = saferead(f, Int16, FEND)
+    firstframe = saferead(f, Int16, FEND)
+    lastframe = saferead(f, Int16, FEND)
+    maxinterp = saferead(f, Int16, FEND)
+    pointscale = saferead(f, FType, FEND)
+    DATA_START = saferead(f, Int16, FEND)
+    analogpframe = saferead(f, Int16, FEND)
+    framerate = saferead(f, FType, FEND)
     seek(f, (148 - 1) * 2)
-    tmp = saferead(f, Int16)
-    tmp_lr = saferead(f, Int16)
+    tmp = saferead(f, Int16, FEND)
+    tmp_lr = saferead(f, Int16, FEND)
     lr = (tmp == 0x3039) ? tmp_lr : nothing
-    char4 = (saferead(f, Int16) == 0x3039)
-    numevents = saferead(f, Int16)
-    read(f,Int16)
-    eventtimes = saferead(f, Float32, 18)
-    eventflags = BitArray(saferead(f, Int8, 18))
-    read(f,Int16)
+    char4 = (saferead(f, Int16, FEND) == 0x3039)
+    numevents = saferead(f, Int16, FEND)
+    read(f, Int16)
+    eventtimes = saferead(f, FType, FEND, 18)
+    eventflags = BitArray(saferead(f, Int8, FEND, 18))
+    read(f, Int16)
 
-    tdata = convert.(Char, saferead(f, UInt8, (4, 18)))
+    tdata = convert.(Char, saferead(f, UInt8, FEND, (4, 18)))
     eventlabels = [ String(tdata[((i - 1) * 4 + 1):(i * 4)]) for i in 1:18]
 
     (length(findall(eventflags)) != numevents) && 1 # They should be the same, header is not authoritative tho, so don't error?
@@ -134,11 +133,11 @@ function readheader(f::IOStream)::Dict{Symbol,Any}
                 :eventlabels => eventlabels)
 end
 
-function readgroup(f::IOStream)
+function readgroup(f::IOStream, FEND::Endian, FType::Type{T}) where T <: Union{Float32,VaxFloatF}
     pos = position(f)
-    nl = saferead(f, Int8)
+    nl = saferead(f, Int8, FEND)
     isLocked = nl < 0 ? true : false
-    gid = saferead(f, Int8)
+    gid = saferead(f, Int8, FEND)
 
     name = transcode(String, read(f, abs(nl)))
     symname = replace(strip(name), r"[^a-zA-Z0-9_]" => '_')
@@ -148,18 +147,18 @@ function readgroup(f::IOStream)
             Unexpected results may occur")
     end
 
-    np = saferead(f, Int16)
-    dl = saferead(f, Int8)
+    np = saferead(f, Int16, FEND)
+    dl = saferead(f, Int8, FEND)
     desc = transcode(String, read(f, dl))
 
     return Group(pos, nl, isLocked, gid, name, symname, np, dl, desc, Dict{Symbol,Array{Parameter,1}}())
 end
 
-function readparam(f::IOStream)
+function readparam(f::IOStream, FEND::Endian, FType::Type{Y}) where Y <: Union{Float32,VaxFloatF}
     pos = position(f)
-    nl = saferead(f, Int8)
+    nl = saferead(f, Int8, FEND)
     isLocked = nl < 0 ? true : false
-    gid = saferead(f, Int8)
+    gid = saferead(f, Int8, FEND)
     # println(nl)
     name = transcode(String, read(f, abs(nl)))
     symname = replace(strip(name), r"[^a-zA-Z0-9_]" => '_')
@@ -169,61 +168,62 @@ function readparam(f::IOStream)
             Unexpected results may occur")
     end
 
-    np = saferead(f, Int16)
+    np = saferead(f, Int16, FEND)
 
-    ellen = saferead(f, Int8)
+    ellen = saferead(f, Int8, FEND)
     if ellen == -1
-        T = String
+        T = TA = String
     elseif ellen == 1
-        T = Int8
+        T = TA = Int8
     elseif ellen == 2
-        T = Int16
+        T = TA = Int16
     elseif ellen == 4
-        T = Float32
+        T = FType
+        TA = Float32
     else
         println("File position in bytes ", position(f))
         println("nl: ", nl, "\ngid: ", gid, "\nname: ", name, "\nnp: ", np, "\nellen: ", ellen)
         error("Invalid parameter element type. Found ", ellen)
     end
 
-    nd = saferead(f, Int8)
+    nd = saferead(f, Int8, FEND)
     if nd > 0
-        dims = NTuple{convert(Int, nd),Int8}(saferead(f, Int8, nd))
+        dims = NTuple{convert(Int, nd),Int8}(saferead(f, Int8, FEND, nd))
         if T == String
-            tdata = convert.(Char, saferead(f, UInt8, convert.(Int, dims)))
+            tdata = convert.(Char, saferead(f, UInt8, FEND, convert.(Int, dims)))
             if nd > 1
                 data = [ String(tdata[((i - 1) * dims[1] + 1):(i * dims[1])]) for i in 1:(*)(dims[2:end]...)]
             else
                 data = [ String(tdata) ]
             end
         else
-            data = saferead(f, T, convert.(Int, dims))
+            data = saferead(f, T, FEND, convert.(Int, dims))
         end
     else
         dims = ()
         if T == String
             data = [ convert(Char, read(f, UInt8)) ]
         else
-            data = [ saferead(f, T) ]
+            data = [ saferead(f, T, FEND) ]
         end
     end
 
-    dl = saferead(f, Int8)
+    dl = saferead(f, Int8, FEND)
     desc = transcode(String, read(f, dl))
 
-    N = nd == 0 ? 1 : convert(Int,nd)
+    N = nd == 0 ? 1 : convert(Int, nd)
 
     if T == String && N > 1
         N -= 1
     end
 
-    return Parameter{T,N}(pos, nl, isLocked, gid, name, symname, np, ellen, nd, dims, data,
+    return Parameter{TA,N}(pos, nl, isLocked, gid, name, symname, np, ellen, nd, dims, data,
             dl, desc)
 end
 
-function readdata(f::IOStream, groups::Dict{Symbol,Group}, header)
+function readdata(f::IOStream, groups::Dict{Symbol,Group}, header, FEND::Endian, FType::Type{T}) where T <: Union{Float32,VaxFloatF}
 
-    format = groups[:POINT][:SCALE][1] > 0 ? Int16 : Float32
+    format = groups[:POINT][:SCALE][1] > 0 ? Int16 : FType
 
     # Read data in a transposed structure for better read/write speeds due to Julia being
     # column-order arrays
@@ -238,10 +238,10 @@ function readdata(f::IOStream, groups::Dict{Symbol,Group}, header)
     dad = Array{Float32,2}(undef, darows,dacols)
 
     for i in 1:d3cols
-        tmp = saferead(f,format,convert(Int,d3rows*4/3))
+        tmp = saferead(f, format, FEND, convert(Int,d3rows*4/3))
         d3d[:,i] = tmp[filter(x -> x % 4 != 0, 1:convert(Int,d3rows*4/3))]
         d3residuals[:,i] = tmp[filter(x -> x % 4 == 0, 1:convert(Int,d3rows*4/3))]
-        dad[:,((i-1)*apf+1):(i*apf)] = saferead(f,format,(darows,apf))
+        dad[:,((i-1)*apf+1):(i*apf)] = saferead(f, format, FEND, (darows,apf))
     end
 
     if format == Int16
@@ -249,69 +249,45 @@ function readdata(f::IOStream, groups::Dict{Symbol,Group}, header)
         d3d *= abs(groups[:POINT][:SCALE][1])
     end
 
-    dad[:] = (dad .- groups[:ANALOG][:OFFSET][1])*
+    dad[:] = (dad .- groups[:ANALOG][:OFFSET][1]).*
                 groups[:ANALOG][:GEN_SCALE][1].*
                 groups[:ANALOG][:SCALE][1:length(groups[:ANALOG][:USED])]
 
     return C3DFile(f.name, header, groups, d3d', dad')
 end
 
-saferead(f::IOStream, T::Union{Type{Int8},Type{UInt8}}) = read(f, T)
-saferead(f::IOStream, T::Union{Type{Int8},Type{UInt8}}, dims) = read!(f, Array{T}(undef, dims...))
+saferead(io::IOStream, ::Type{T}, FEND::Endian) where T <: Union{Int8,UInt8} = read(io, T)
+saferead(io::IOStream, ::Type{T}, FEND::Endian, dims) where T <: Union{Int8,UInt8} = read!(io, Array{T}(undef, dims...))
 
-function saferead(f::IOStream, T::Type{Float32})
-    if VAX
-        if H_ENDIAN === F_ENDIAN
-            return convert(Float32,read(f, VaxFloatF))
-        else
-            return convert(Float32,ltoh(read(f, VaxFloatF)))
-        end
-    end
-
-    if H_ENDIAN === F_ENDIAN
-        return read(f, T)
-    elseif F_ENDIAN === LE
-        return ltoh(read(f, T))
-    elseif F_ENDIAN === BE
-        return ntoh(read(f, T))
+function saferead(io::IOStream, ::Type{T}, FEND::Endian) where T
+    if FEND == LE
+        return ltoh(read(io, T))
+    else
+        return ntoh(read(io, T))
     end
 end
 
-function saferead(f::IOStream, T::Type{Float32}, dims)
-    if VAX
-        if H_ENDIAN === F_ENDIAN
-            return convert.(Float32,read!(f, Array{VaxFloatF}(undef, dims...)))
-        else
-            return convert.(Float32,ltoh.(read!(f, Array{VaxFloatF}(undef, dims...))))
-        end
-    end
-
-    if H_ENDIAN === F_ENDIAN
-        return read!(f, Array{T}(undef, dims))
-    elseif F_ENDIAN === LE
-        return ltoh.(read!(f, Array{T}(undef, dims)))
-    elseif F_ENDIAN === BE
-        return ntoh.(read!(f, Array{T}(undef, dims)))
+function saferead(io::IOStream, ::Type{T}, FEND::Endian, dims) where T
+    if FEND == LE
+        return ltoh.(read!(io, Array{T}(undef, dims)))
+    else
+        return ntoh.(read!(io, Array{T}(undef, dims)))
     end
 end
 
-function saferead(f::IOStream, T::Type)
-    if H_ENDIAN === F_ENDIAN
-        return read(f, T)
-    elseif F_ENDIAN === LE
-        return ltoh(read(f, T))
-    elseif F_ENDIAN === BE
-        return ntoh(read(f, T))
+function saferead(io::IOStream, ::Type{VaxFloatF}, FEND::Endian)
+    if FEND == LE
+        return convert(Float32, ltoh(read(io, VaxFloatF)))
+    else
+        return convert(Float32, ntoh(read(io, VaxFloatF)))
     end
 end
 
-function saferead(f::IOStream, T::Type, dims)
-    if H_ENDIAN === F_ENDIAN
-        return read!(f, Array{T}(undef, dims...))
-    elseif F_ENDIAN === LE
-        return ltoh.(read!(f, Array{T}(undef, dims...)))
-    elseif F_ENDIAN === BE
-        return ntoh.(read!(f, Array{T}(undef, dims...)))
+function saferead(io::IOStream, ::Type{VaxFloatF}, FEND::Endian, dims)
+    if FEND == LE
+        return convert.(Float32, ltoh.(read!(io, Array{VaxFloatF}(undef, dims))))
+    else
+        return convert.(Float32, ntoh.(read!(io, Array{VaxFloatF}(undef, dims))))
     end
 end
 
@@ -322,6 +298,16 @@ function readc3d(filename::AbstractString)
 
     file = open(filename, "r")
 
+    groups, header, FEND, FType = _readparams(file)
+
+    res = readdata(file, groups, header, FEND, FType)
+
+    close(file)
+
+    return res
+end
+
+function _readparams(file::IOStream)
     params_ptr = read(file, UInt8)
 
     if read(file, UInt8) != 0x50
@@ -338,25 +324,25 @@ function readc3d(filename::AbstractString)
     paramblocks = read(file, UInt8)
     proctype = read(file, Int8) - 83
 
-    global VAX = false
+    FType = Float32
 
     # Deal with host big-endianness in the future
     if proctype == 1
         # little-endian
-        global F_ENDIAN = LE
+        FEND = LE
     elseif proctype == 2
         # DEC floats; little-endian
-        global VAX = true
-        global F_ENDIAN = LE
+        FType = VaxFloatF
+        FEND = LE
     elseif proctype == 3
         # big-endian
-        global F_ENDIAN = BE
+        FEND = BE
     else
         error("Malformed processor type. Expected 1, 2, or 3. Found ", proctype)
     end
 
     mark(file)
-    header = readheader(file)
+    header = readheader(file, FEND, FType)
     reset(file)
 
     gs = Array{Group,1}()
@@ -367,12 +353,12 @@ function readc3d(filename::AbstractString)
     if read(file, Int8) < 0
         # Group
         relseek(file, -2)
-        push!(gs, readgroup(file))
+        push!(gs, readgroup(file, FEND, FType))
         moreparams = gs[end].np != 0 ? true : false
     else
         # Parameter
         relseek(file, -2)
-        push!(ps, readparam(file))
+        push!(ps, readparam(file, FEND, FType))
         moreparams = ps[end].np != 0 ? true : false
     end
 
@@ -381,11 +367,11 @@ function readc3d(filename::AbstractString)
         local gid = read(file, Int8)
         if gid < 0 # Group
             relseek(file, -2)
-            push!(gs, readgroup(file))
+            push!(gs, readgroup(file, FEND, FType))
             moreparams = gs[end].np != 0 ? true : false
         elseif gid > 0 # Parameter
             relseek(file, -2)
-            push!(ps, readparam(file))
+            push!(ps, readparam(file, FEND, FType))
             moreparams = ps[end].np != 0 ? true : false
         else # Last parameter pointer is incorrect (assumption)
             # The group ID should never be zero, if it is, the most likely explanation is
@@ -420,11 +406,7 @@ function readc3d(filename::AbstractString)
         groups[gids[param.gid]].p[Symbol(param.symname)] = param
     end
 
-    res = readdata(file,groups, header)
-
-    close(file)
-
-    return res
+    return (groups, header, FEND, FType)
 end
 
 function readparams(filename::AbstractString)
@@ -434,117 +416,11 @@ function readparams(filename::AbstractString)
 
     file = open(filename, "r")
 
-    params_ptr = read(file, UInt8)
-
-    if read(file, UInt8) != 0x50
-        error("File ", filename, " is not a valid C3D file")
-    end
-
-    # Jump to parameters block
-    seek(file, (params_ptr - 1) * 512)
-
-    # Skip 2 reserved bytes
-    # TODO: store bytes for saving modified files
-    read(file, UInt16)
-
-    paramblocks = read(file, UInt8)
-    proctype = read(file, Int8) - 83
-
-    global VAX = false
-
-    # Deal with host big-endianness in the future
-    if proctype == 1
-        # little-endian
-        global F_ENDIAN = LE
-    elseif proctype == 2
-        # DEC floats; little-endian
-        global VAX = true
-        global F_ENDIAN = LE
-    elseif proctype == 3
-        # big-endian
-        global F_ENDIAN = BE
-    else
-        error("Malformed processor type. Expected 1, 2, or 3. Found ", proctype)
-    end
-
-    mark(file)
-    header = readheader(file)
-    reset(file)
-
-    gs = Array{Group,1}()
-    ps = Array{Parameter,1}()
-    moreparams = true
-
-    read(file, UInt8)
-    if read(file, Int8) < 0
-        # Group
-        relseek(file, -2)
-        push!(gs, readgroup(file))
-        moreparams = gs[end].np != 0 ? true : false
-    else
-        # Parameter
-        relseek(file, -2)
-        push!(ps, readparam(file))
-        moreparams = ps[end].np != 0 ? true : false
-    end
-
-    while moreparams
-        read(file, UInt8)
-        local gid = read(file, Int8)
-        if gid < 0 # Group
-            relseek(file, -2)
-            push!(gs, readgroup(file))
-            moreparams = gs[end].np != 0 ? true : false
-        elseif gid > 0 # Parameter
-            relseek(file, -2)
-            push!(ps, readparam(file))
-            moreparams = ps[end].np != 0 ? true : false
-        else # Last parameter pointer is incorrect (assumption)
-            # The group ID should never be zero, if it is, the most likely explanation is
-            # that the pointer is incorrect (ie the end of the parameters has been reached
-            # and the remaining 0x00's are fill to the end of the block
-
-            # Check if pointer is incorrect
-            relseek(file, -2)
-            mark(file)
-
-            local z = read(file, (((params_ptr + paramblocks) - 1) * 512) - position(file))
-
-            if isempty(find(!iszero, z))
-                unmark(file)
-                moreparams = false
-            else
-                reset(file)
-                error("Invalid group id at byte ", position(file) + 1)
-            end
-        end
-    end
-
-    groups = Dict{Symbol,Group}()
-    gids = Dict{Int,Symbol}()
-
-    for group in gs
-        groups[Symbol(group.symname)] = group
-        gids[abs(group.gid)] = Symbol(group.symname)
-    end
-
-    for param in ps
-        groups[gids[param.gid]].p[Symbol(param.symname)] = param
-    end
+    groups, header, FEND, FType = _readparams(file)
 
     close(file)
 
     return groups
-end
-
-function __init__()
-    if ENDIAN_BOM == 0x04030201
-        global const H_ENDIAN = LE
-    elseif ENDIAN_BOM == 0x01020304
-        global const H_ENDIAN = BE
-    else
-        error("Weird endianness error")
-    end
 end
 
 end # module
