dbf33cf43417d6b367d46161fc3f0fbe896b260a
Allen Hill <allenofthehills@gmail.com>
2024-08-05 16:04:25 -0700

    Refactor tests to reduce redundancy and improve readability

--- test/publicinterface.jl
+++ test/publicinterface.jl
@@ -1,3 +1,40 @@
+const readable_files = [
+    artifact"sample15/FP1.C3D"
+    artifact"sample15/FP2.C3D"
+    artifact"sample17/128analogchannels.c3d"
+    artifact"sample19/sample19.c3d"
+    artifact"sample31/large01.c3d"
+    artifact"sample31/large02.c3d"
+    artifact"sample33/bigparlove.c3d"
+    artifact"sample01/Eb015pr.c3d"
+    artifact"sample03/gait-pig.c3d"
+    artifact"sample00/Vicon Motion Systems/TableTennis.c3d"
+    artifact"sample36/18124framesi.c3d"
+    artifact"sample36/18124framesf.c3d"
+    artifact"sample36/36220framesi.c3d"
+    artifact"sample36/36220framesf.c3d"
+    artifact"sample36/72610framesi.c3d"
+    artifact"sample36/72610framesf.c3d"
+    artifact"sample36/18124framesi.c3d"
+    artifact"sample01/Eb015pr.c3d"
+    artifact"sample16/basketball.c3d"
+    artifact"sample16/giant.c3d"
+    artifact"sample16/golf.c3d"
+    artifact"sample29/Facial-Sing.c3d"
+    artifact"sample29/OptiTrack-IITSEC2007.c3d"
+    artifact"sample34/AlbertoINT.c3d"
+    artifact"sample34/AlbertoREAL.c3d"
+    artifact"sample34/Basketball.c3d"
+    artifact"sample35/Mega Electronics Isokinetic EMG Angle Torque Sample File.c3d"
+    artifact"sample01/Eb015pr.c3d"
+    artifact"sample03/gait-pig.c3d"
+    artifact"sample03/gait-pig-nz.c3d"
+    artifact"sample01/Eb015pr.c3d"
+    artifact"sample18/bad_parameter_section.c3d"
+    artifact"sample27/kyowadengyo.c3d"
+    artifact"sample24/MotionMonitorC3D.c3d"
+]
+
 @testset "readc3d kwargs, etc" begin
     @test_nothrow readc3d(artifact"sample01/Eb015pr.c3d"; paramsonly=true)
     @test_nothrow readc3d(artifact"sample01/Eb015pr.c3d"; paramsonly=true, validate=false)
@@ -22,73 +59,10 @@
     @test show(devnull, MIME("text/plain"), f) == nothing
 
     @testset "Show" begin
-        @test show(devnull, readc3d(artifact"sample15/FP1.C3D")) === nothing
-        @test show(devnull, readc3d(artifact"sample15/FP2.C3D")) === nothing
-        @test show(devnull, readc3d(artifact"sample17/128analogchannels.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample19/sample19.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample31/large01.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample31/large02.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample33/bigparlove.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample01/Eb015pr.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample03/gait-pig.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample00/Vicon Motion Systems/TableTennis.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample36/18124framesi.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample36/18124framesf.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample36/36220framesi.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample36/36220framesf.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample36/72610framesi.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample36/72610framesf.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample36/18124framesi.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample01/Eb015pr.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample16/basketball.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample16/giant.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample16/golf.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample29/Facial-Sing.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample29/OptiTrack-IITSEC2007.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample34/AlbertoINT.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample34/AlbertoREAL.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample34/Basketball.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample35/Mega Electronics Isokinetic EMG Angle Torque Sample File.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample01/Eb015pr.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample03/gait-pig.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample03/gait-pig-nz.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample01/Eb015pr.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample18/bad_parameter_section.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample27/kyowadengyo.c3d")) === nothing
-        @test show(devnull, readc3d(artifact"sample24/MotionMonitorC3D.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample15/FP1.C3D")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample15/FP2.C3D")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample17/128analogchannels.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample19/sample19.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample31/large01.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample31/large02.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample33/bigparlove.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample01/Eb015pr.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample03/gait-pig.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample00/Vicon Motion Systems/TableTennis.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample36/18124framesi.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample36/18124framesf.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample36/36220framesi.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample36/36220framesf.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample36/72610framesi.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample36/72610framesf.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample36/18124framesi.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample01/Eb015pr.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample16/basketball.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample16/giant.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample16/golf.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample29/Facial-Sing.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample29/OptiTrack-IITSEC2007.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample34/AlbertoINT.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample34/AlbertoREAL.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample34/Basketball.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample35/Mega Electronics Isokinetic EMG Angle Torque Sample File.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample01/Eb015pr.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample03/gait-pig.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample03/gait-pig-nz.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample01/Eb015pr.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample18/bad_parameter_section.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample27/kyowadengyo.c3d")) === nothing
-        @test show(devnull, MIME"text/plain"(), readc3d(artifact"sample24/MotionMonitorC3D.c3d")) === nothing
+        @testset "$file" for file in readable_files
+            @test_internalconsistency readc3d(file)
+            @test_nothrow show(devnull, readc3d(file))
+            @test_nothrow show(devnull, MIME"text/plain"(), readc3d(file))
+        end
     end
 end
