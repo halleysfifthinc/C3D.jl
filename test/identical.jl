693229d26395d600536688131dbd0c52636b45fb
Allen Hill <allenofthehills@gmail.com>
2024-08-12 15:11:48 -0700

    Refactor identical file comparison helper function and tests

--- test/identical.jl
+++ test/identical.jl
@@ -29,14 +29,37 @@
         end
     end
 
-    @testset "sample01 files" begin
-        comparefiles.(Ref(joinpath(artifact"sample01", sample01[1])), joinpath.(Ref(artifact"sample01"), sample01[2:end]))
+    @testset "sample01" begin
+        reference = joinpath(artifact"sample01", sample01[1])
+        @testset "$(basename(reference)) vs. $(basename(candidate))" for candidate in sample01[2:end]
+            comparefiles(reference, joinpath(artifact"sample01", candidate))
+        end
     end
     @testset "sample02 files" begin
-        comparefiles.(Ref(joinpath(artifact"sample02", sample02[1])), joinpath.(Ref(artifact"sample02"), sample02[2:end]))
+        reference = joinpath(artifact"sample02", sample02[1])
+        @testset "$(basename(reference)) vs. $(basename(candidate))" for candidate in sample02[2:end]
+            if candidate == "dec_int.c3d"
+                # the dec_int.c3d file has a smattering of incorrect camera values for the
+                # listed points (ie confirmed by looking at the binary, differs from the
+                # other 5)
+                # Example using "LFT2", the first sample is 0x1930-0x193F in pc_real.c3d and
+                # 0x1898-0x189F in dec_int.c3d. The residual is stored at 0x193C and 0x189E
+                # and is 0x46642400 and 0x3a09, respectively.
+                # `(UInt16(reinterpret(Float32, 0x46642400)) == 0x3909) != 0x3a09`
+                comparefiles(reference, joinpath(artifact"sample02", candidate);
+                    ignore_cameras=["RFT2", "RSK1", "RSK2", "RSK3", "RTH1", "RTH3", "RPV2",
+                        "LSK1", "LSK2", "LFT1", "LFT2", "RAR3", "RFA3", "LAR3", "LFA2",
+                        "LFA3"])
+            else
+                comparefiles(reference, joinpath(artifact"sample02", candidate))
+            end
+        end
     end
     @testset "sample08 files" begin
-        comparefiles.(Ref(joinpath(artifact"sample08", sample08[1])), joinpath.(Ref(artifact"sample08"), sample08[2:end]))
+        reference = joinpath(artifact"sample08", sample08[1])
+        @testset "$(basename(reference)) vs. $(basename(candidate))" for candidate in sample08[2:end]
+            comparefiles(reference, joinpath(artifact"sample08", candidate))
+        end
     end
 end
 
